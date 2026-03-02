1f13ba645afce085f159bf731e9f02241f61d980
Bra1nst0rmm <krasnovskyi2021@mail.ru>
2026-03-02 16:13:59 +0300

    /

--- script.sh
+++ script.sh
@@ -11,7 +11,7 @@ SSH_DIR="/root/.ssh"
 mkdir -p $SSH_DIR
 
 # Добавляем ключ в authorized_keys
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC0p8lUtjoHqMA+lCWV6Vs2/pE9AEPvv1A5G4cKufYNxIZwGytz6hIUcIgR941HfRWOUJqZV6+WlBjva2gsLzXwHfIvlguy8uHgyq27rjbLnnmoC5wjjqT67cymj6mMB0DkeZhWBF1rYzKKynfU7IMVcHdYISfeK6a71GGiqzv7fKIl6x9wRuL4WzpQ26krDl7vJMZz/E9w3faSItU/LVHlUGPSYlYHUjpahNLq5O9T5f6v6SR9mXRFqUhiXTfRq4o8NKzlx42a5g16UVvDMPc8uk78vr9i0ozW43T7tT0oZ/LS0jWGWQpdSHPrqSrf18j3is6bW3VhNfOP1TfvTAKsi0ImshN095LSCBdORrTQpLhqCFbBuGntoJQLv1EL2GBjOLtXdsqEhw/O8XlvG55YkensRmlWfdhUCU0oDuvt1Q0e35EnJ+R6mDCmMZ4LqrdidqnUBj1dlAMkHEjyrDZOGPgqwKKy2BVBEnirKsVLIYHj109sel100YCVaAxDV4L+mYsM5c7CbV60bttJSPc6wuwRjU4Zdy93kd+cpnp/ClTpA+phcpqLEW5z7aL1qYkbAsCQ0mbzy2OjJ/paxSvZg64++f0TVlHPNKg50qNA5V7s+giCP7xxJgM65SfVyPUs0f1Pld6TQ2/nNjywTiVSvR0+/x5V0gw5mHpysmLRAQ== archi@MacBook-Pro-Mikhail.local" >> $SSH_DIR/authorized_keys
+echo "ssh-rsa (rsa-key)== name@user.local" >> $SSH_DIR/authorized_keys
 
 sudo apt install openssh-server -y
 
